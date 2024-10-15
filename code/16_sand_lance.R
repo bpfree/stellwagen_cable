9b5159d1220a41dfe1a53ee7780df8790193b0ae
bpfree <brian.patrick.free@gmail.com>
2024-10-15 15:49:54 -0400

    update script order number

---
+++ code/16_sand_lance.R
@@ -0,0 +1,135 @@
+#######################################
+### 16. critical sand lance habitat ###
+#######################################
+
+# clear environment
+rm(list = ls())
+
+# calculate start time of code (determine how long it takes to complete all code)
+start <- Sys.time()
+
+#####################################
+#####################################
+
+# set parameters
+## designate region name
+region_name <- "stellwagen"
+
+## layer names
+data_name <- "sand_lance"
+
+## coordinate reference system
+### set the coordinate reference system that data should become (NAD83 UTM 19N: https://epsg.io/26919)
+crs <- "EPSG:26919"
+
+## designate date
+date <- format(Sys.Date(), "%Y%m%d")
+
+#####################################
+#####################################
+
+# load packages
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(renv,
+               dplyr,
+               ggplot2,
+               janitor,
+               plyr,
+               purrr,
+               rmapshaper,
+               sf,
+               sp,
+               stringr,
+               targets,
+               terra, # is replacing the raster package
+               tidyr)
+
+#####################################
+#####################################
+
+# set directories
+## define data directory (as this is an R Project, pathnames are simplified)
+### input directories
+#### sand lance
+data_dir <- "data/a_raw_data/Sand_Lance_FGDB.gdb"
+
+#### study area grid
+study_region_gpkg <- stringr::str_glue("data/a_raw_data/{region_name}.gpkg")
+
+### output directories
+#### costs geopackage
+output_gpkg <- "data/b_intermediate_data/costs.gpkg"
+
+#####################################
+
+# inspect layers within geodatabases and geopackages
+sf::st_layers(dsn = data_dir,
+              do_count = T)
+
+sf::st_layers(dsn = study_region_gpkg,
+              do_count = T)
+
+#####################################
+#####################################
+
+# read data
+## sand lance
+data <- sf::st_read(dsn = data_dir,
+                    layer = sf::st_layers(dsn = data_dir)[[1]][grep(pattern = stringr::str_glue("{data_name}"),
+                                                                    x = sf::st_layers(dsn = data_dir)[[1]])]) %>%
+  sf::st_transform(x = .,
+                   crs = crs)
+
+### Check units for data
+sf::st_crs(data, parameters = TRUE)$units_gdal
+
+## Stellwagen region
+region <- sf::st_read(dsn = study_region_gpkg,
+                      layer = stringr::str_glue("{region_name}_region")) %>%
+  sf::st_transform(x = .,
+                   crs = crs)
+
+## Stellwagen grid
+grid <- sf::st_read(dsn = study_region_gpkg,
+                    layer = stringr::str_glue("{region_name}_grid")) %>%
+  sf::st_transform(x = .,
+                   crs = crs)
+
+#####################################
+#####################################
+
+# limit data to study region
+data_region <- data %>%
+  rmapshaper::ms_clip(target = .,
+                      clip = region) %>%
+  dplyr::mutate(layer = stringr::str_glue("{data_name}")) %>%
+  dplyr::group_by(layer) %>%
+  dplyr::summarise()
+
+#####################################
+#####################################
+
+# sand lance grid
+data_region_grid <- grid[data_region, ] %>%
+  # spatially join sand lance to Stellwagen grid
+  sf::st_join(x = .,
+              y = data_region,
+              join = st_intersects) %>%
+  # select fields of importance
+  dplyr::select(index, layer)
+
+#####################################
+#####################################
+
+# export data
+## costs geopackage
+sf::st_write(obj = data_region_grid, dsn = output_gpkg, layer = stringr::str_glue("{region_name}_{data_name}_grid"), append = FALSE)
+
+## intermediate geopackage
+
+
+#####################################
+#####################################
+
+# calculate end time and print time difference
+print(Sys.time() - start) # print how long it takes to calculate

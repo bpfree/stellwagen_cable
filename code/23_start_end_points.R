9795e990fd57d23d3037f0b2497d28948af45736
bpfree <brian.patrick.free@gmail.com>
2024-12-22 19:20:55 -0500

    update start and end options along Stellwagen boundary; delete unnecessary old code

--- code/23_start_end_points.R
+++ code/23_start_end_points.R
@@ -195,49 +195,34 @@ plot(stellwagen_end$x)
 
 #####################################
 
-# a <- stellwagen_start %>%
-#   sf::st_drop_geometry() %>%
-#   dplyr::select(lon, lat) %>%
-#   dplyr::rename(starts_x = lon, starts_y = lat)
-# 
-# b <- stellwagen_end %>%
-#   sf::st_drop_geometry() %>%
-#   dplyr::select(lon, lat) %>%
-#   dplyr::rename(ends_x = lon, ends_y = lat)
-
-a2 <- stellwagen_start %>%
+a <- stellwagen_start %>%
   sf::st_drop_geometry() %>%
   dplyr::select(lon, lat) %>%
   dplyr::rename(starts_x = lon, starts_y = lat) %>%
   # create start index
   dplyr::mutate(index_start = row_number())
 
-b2 <- stellwagen_end %>%
+b <- stellwagen_end %>%
   sf::st_drop_geometry() %>%
   dplyr::select(lon, lat) %>%
   dplyr::rename(ends_x = lon, ends_y = lat) %>%
   # create start index
   dplyr::mutate(index_end = 77 + row_number())
 
-# pairs <- tidyr::crossing(a, b) %>%
-#   dplyr::mutate(index = row_number()) %>%
-#   dplyr::relocate(index, .before = starts_x)
-# 
-# View(pairs)
+pairs <- tidyr::crossing(a, b) %>%
+  dplyr::mutate(index = row_number()) %>%
+  dplyr::relocate(index, .before = starts_x)
 
-for(i in seq(nrow(a2))){
-  for(j in seq(nrow(b2))){
+View(pairs)
+
+for(i in seq(nrow(a))){
+  for(j in seq(nrow(b))){
     
-    # row <- as.numeric(c(a2[i,], b2[j,]))
-    # row <- matrix(as.numeric(c(a2[i,], b2[j,])), ncol = 3, byrow = TRUE)
-    # row <- st_linestring(matrix(as.numeric(c(a2[i,1:2], b2[j,1:2])), ncol = 2, byrow = TRUE))
-    # row <- st_sfc(st_linestring(matrix(as.numeric(c(a2[i,1:2], b2[j,1:2])), ncol = 4, byrow = TRUE)), crs = crs)
-    
-    row <- sf::st_as_sf(sf::st_sfc(sf::st_linestring(matrix(as.numeric(c(a2[i, 1:2], b2[j, 1:2])), ncol = 2, byrow = TRUE)), crs = crs))
+    row <- sf::st_as_sf(sf::st_sfc(sf::st_linestring(matrix(as.numeric(c(a[i, 1:2], b[j, 1:2])), ncol = 2, byrow = TRUE)), crs = crs))
     
     row <- sf::st_set_geometry(row, "geometry")
     
-    row <- cbind(row, a2[i,], b2[j,])
+    row <- cbind(row, a[i,], b[j,])
     
     if(i==1 & j==1){out_df = row}
     else{out_df = rbind(out_df, row)}
@@ -251,29 +236,6 @@ out_df$geometry[1,]
 View(out_df)
 plot(out_df$geometry)
 
-# for(i in seq(nrow(a))){
-#   for(j in seq(nrow(b))){
-#     
-#     row <- as.numeric(c(a[i,], b[j,]))
-#     row <- matrix(as.numeric(c(a[i,], b[j,])), ncol = 2, byrow = TRUE)
-#     row <- st_linestring(matrix(as.numeric(c(a[i,], b[j,])), ncol = 2, byrow = TRUE))
-#     row <- st_as_sf(st_sfc(st_linestring(matrix(as.numeric(c(a[i,], b[j,])), ncol = 2, byrow = TRUE)), crs = crs))
-#     
-#     row <- st_set_geometry(row, "geometry")
-#     
-#     row <- cbind(row, a[i,], b[j,])
-# 
-#     if(i==1 & j==1){out_df = row}
-#     else{out_df = rbind(out_df, row)}
-#   }
-# }
-# 
-# row
-# out_df$geometry[1,]
-# 
-# View(out_df)
-# plot(out_df$geometry)
-
 g <- ggplot2::ggplot() +
   ggplot2::geom_sf(data = stellwagen_start, color = "lightblue", size = 2) +
   ggplot2::geom_sf(data = stellwagen_end, color = "darkred", size = 2) +
@@ -284,28 +246,10 @@ g
 write.csv(x = pairs, file = file.path(csv_dir, "stellwagen_points.csv"))
 sf::st_write(obj = out_df, dsn = output_gpkg, layer = "stellwagen_lines", append = F)
 
-# sf::st_delete(dsn = output_gpkg, layer = "stellwagen_lines")
-
 #####################################
 #####################################
 
-# end point
-# end_points <- sf::st_read(dsn = data_dir,
-#                            layer = sf::st_layers(dsn = data_dir)[[1]][grep(pattern = "2_2_corridors_end",
-#                                                                            x = sf::st_layers(dsn = data_dir)[[1]])]) %>%
-#   sf::st_zm() %>%
-#   sf::st_cast("POINT") %>%
-#   sf::st_transform(x = .,
-#                    crs = crs)
-# 
-# boston <- end_points %>%
-#   dplyr::slice(2)
-# 
-# plymouth <- end_points %>%
-#   dplyr::slice(1)
-
-#####################################
-
+# end points
 end_point <- rbind(c("point",-70.995, 42.341), # Boston
                    c("point",-70.623, 41.980)) %>% # Plymouth
   # convert to data frame
@@ -369,7 +313,7 @@ sf::st_write(lease_0567_edge_point, dsn = output_gpkg, layer = stringr::str_glue
 sf::st_write(stellwagen_start, dsn = output_gpkg, layer = stringr::str_glue("{region_name}_stellwagen_starts"), append = FALSE)
 sf::st_write(stellwagen_end, dsn = output_gpkg, layer = stringr::str_glue("{region_name}_stellwagen_ends"), append = FALSE)
 
-# sf::st_write(end_points, dsn = output_gpkg, layer = stringr::str_glue("{region_name}_end_points"), append = FALSE)
+sf::st_write(end_point, dsn = output_gpkg, layer = stringr::str_glue("{region_name}_end_points"), append = FALSE)
 sf::st_write(plymouth, dsn = output_gpkg, layer = stringr::str_glue("{region_name}_plymouth_end_point"), append = FALSE)
 sf::st_write(boston, dsn = output_gpkg, layer = stringr::str_glue("{region_name}_boston_end_point"), append = FALSE)
 
